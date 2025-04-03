a5ce18ddf62ec89766d361ed60efbef062c0eded
Somesh Pandey <spandey5368@gmail.com>
2025-04-04 00:48:44 +0530

    home screen fix

--- src/component/Chats/Chats.jsx
+++ src/component/Chats/Chats.jsx
@@ -6,7 +6,7 @@ import Contact from '../Contact/Contact';
 
 function Chats() {
   return (
-    <div className="h-auto w-full md:w-[50%] lg:w-[25%] border-2 border-opacity-25 border-slate-950 p-2">
+    <div className="h-full  md:w-[50%] lg:w-[25%] border-2 border-opacity-25 border-slate-950 p-1">
       <div className="chats w-full h-12 flex justify-between items-center mx-2">
         <span className="font-bold text-xl md:text-2xl font-medium">Chats</span>
         <span className="text-2xl md:text-2xl pr-4"><FontAwesomeIcon icon={faPlus} /></span>
